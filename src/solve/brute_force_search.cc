b46109fdeeb00fb8c815974088b77ba76b6da1f5
Matthew Burke <matthelb@usc.edu>
2014-08-18 22:41:11 -0400

    Implement BruteForceSearch algorithm for solving TSP Graphs

---
+++ src/solve/brute_force_search.cc
@@ -0,0 +1,63 @@
+#include "solve/brute_force_search.h"
+
+#include <iostream>
+
+using namespace std;
+
+BruteForceSearch::BruteForceSearch() {
+}
+
+BruteForceSearch::~BruteForceSearch() {
+}
+
+void BruteForceSearch::SearchPermutations(
+  vector<int> current_path, int current_distance,
+  unordered_set<int> unvisited_nodes) {
+  if (unvisited_nodes.size() == 0) {
+    if (current_path.size() == graph()->num_nodes()) {
+      current_distance += graph()->GetEdgeWeight(current_path.back(),
+                                                 current_path.front());
+      if (current_distance < shortest_distance_ || shortest_path_.size() == 0) {
+        shortest_path_ = current_path;
+        shortest_distance_ = current_distance;
+        cout << current_distance << endl;
+      }
+    }
+    return;
+  }
+  for (unordered_set<int>::iterator itr = unvisited_nodes.begin();
+       itr != unvisited_nodes.end();
+       ++itr) {
+    int node_j = *itr;
+    int weight = 0;
+    if (current_path.size() > 0) {
+      weight = graph()->GetEdgeWeight(current_path.back(), node_j);
+    }
+    if (current_path.size() == 0 || weight) {
+      vector<int> new_current_path = current_path;
+      new_current_path.push_back(node_j);
+      int new_current_distance = current_distance + weight;
+      unordered_set<int> new_unvisited_nodes = unvisited_nodes;
+      new_unvisited_nodes.erase(new_unvisited_nodes.find(node_j));
+      SearchPermutations(new_current_path, new_current_distance, new_unvisited_nodes);
+    }
+  }
+}
+
+pair<vector<int>, int> BruteForceSearch::OptimalPath(const Graph* graph) {
+  Reset(graph);
+  vector<int> current_path;
+  int current_distance = 0;
+  unordered_set<int> unvisited_nodes;
+  for (unsigned int i = 0; i < this->graph()->num_nodes(); ++i) {
+    unvisited_nodes.insert(i);
+  }
+  SearchPermutations(current_path, current_distance, unvisited_nodes);
+  return make_pair(shortest_path_, shortest_distance_);
+}
+
+void BruteForceSearch::Reset(const Graph* graph) {
+  graph_ = graph;
+  shortest_path_.clear();
+  shortest_distance_ = 0;
+}
---
+++ src/solve/brute_force_search.h
@@ -0,0 +1,32 @@
+#ifndef TSP_SOLVE_BRUTE_FORCE_SEARCH_H_
+#define TSP_SOLVE_BRUTE_FORCE_SEARCH_H_
+
+#include <unordered_set>
+#include <vector>
+
+#include "graph/graph.h"
+#include "solve/tsp_algorithm.h"
+
+class BruteForceSearch : public TSPAlgorithm {
+ public:
+  BruteForceSearch();
+  virtual ~BruteForceSearch();
+
+  std::pair<std::vector<int>, int> OptimalPath(const Graph* graph);
+
+ private:
+  BruteForceSearch(BruteForceSearch& brute_force_search);
+  void operator=(BruteForceSearch& brute_force_search);
+
+  const Graph* graph() const { return graph_; }
+
+  void Reset(const Graph* graph);
+  void SearchPermutations(std::vector<int> current_path, int current_distance,
+                           std::unordered_set<int> unvisited_nodes);
+
+  const Graph* graph_;
+  int shortest_distance_ = 0;
+  std::vector<int> shortest_path_;
+};
+
+#endif
