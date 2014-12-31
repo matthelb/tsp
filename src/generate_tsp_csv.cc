84546089abdcc2a616ecd1651fb63c9f9ee33aba
Matthew Burke <matthelb@usc.edu>
2014-12-30 23:35:12 -0500

    Add processors argument to generate_csv_tsp executable

--- src/generate_tsp_csv.cc
+++ src/generate_tsp_csv.cc
@@ -8,16 +8,16 @@
 
 using namespace std;
 int main(int argc, char* argv[]) {
-  if(argc != 11) {
+  if(argc != 12) {
     cerr << "Usage: " << argv[0] << " <output_dir> <iterations> <min_coord> <max_coord>"
          << " <trials_start> <trials_end> <input_file> <run_id>"
-         << " <max_compute_time> <max_chunk_size>" << endl;
+         << " <max_compute_time> <max_chunk_size> <processors>" << endl;
     return 1;
   }
   string filename = string(argv[1]);
   int iterations = atoi(argv[2]);
   if (iterations <= 0) {
-    cout << "Iterations must be an integer greater than 0" << endl;
+    cerr << "Iterations must be an integer greater than 0" << endl;
     return 1;
   }
   int min_coord = atoi(argv[3]);
@@ -26,24 +26,31 @@ int main(int argc, char* argv[]) {
   int trials_start = atoi(argv[5]);
   int trials_end = atoi(argv[6]);
   if (trials_start < 0 || trials_end <= trials_start) {
-    cout << "Trials start must be an integer greater than 0 and less than "
+    cerr << "Trials start must be an integer greater than 0 and less than "
          << "trials end" << endl;
     return 1;
   }
   int run_id = atoi(argv[8]);
   double max_compute_time = atoi(argv[9]);
   int maxchunksize = atoi(argv[10]);
+  int processors = atoi(argv[11]);
   TSP tsp;
   if (!tsp.Parse(argv[7])) {
     cerr << "Unable to parse TSPLIB file " << argv[7] << endl;
     return 1;
   }
-  ParallelConcordeSolver concorde_solver;
-  concorde_solver.set_maxchunksize(maxchunksize);
-  concorde_solver.set_processors(4);
+  ConcordeSolver* algorithm = NULL;
+  if (processors > 1) {
+    algorithm = new ParallelConcordeSolver();
+    static_cast<ParallelConcordeSolver*>(algorithm)->set_processors(processors);
+  } else {
+    algorithm = new ConcordeSolver();
+  }
+  algorithm->set_maxchunksize(maxchunksize);
   TwoNodeReplacement simulator(run_id, filename, 0, min_coord, max_coord,
-                               true, trials_start, trials_end,
-                               &concorde_solver, max_compute_time);
+                               true, trials_start, trials_end, algorithm,
+                               max_compute_time);
   simulator.Simulate(&tsp, iterations);
+  delete algorithm;
   return 0;
 }
--- src/solve/parallel_concorde_solver.cc
+++ src/solve/parallel_concorde_solver.cc
@@ -26,7 +26,7 @@ int ParallelConcordeSolver::RunConcorde(int ncount, CCdatagroup* dat,
 																				char* name, double* timebound,
 																				int* hit_timebound, int silent) {
 	int process_id = -1;
-  for (int i = 0; i < processors(); ++i) {
+  for (int i = 0; i < processors() - 1; ++i) {
 		process_id = fork();
 		if (process_id == 0) {
 			break;
@@ -41,7 +41,7 @@ int ParallelConcordeSolver::RunConcorde(int ncount, CCdatagroup* dat,
 		while (res_check.fail()) {
 			res_check.open(res_file);
 		}
-		cout << "Starting grunt" << endl;
+		cerr << "Starting grunt" << endl;
 		//char* argv[] = {"/usr/local/src/concorde/TSP/concorde","-g","127.0.0.1", NULL};
 		//execvp(argv[0], argv);
 		CCtsp_grunt("127.0.0.1", CCtsp_HOST_PORT, NULL, NULL, NULL, 0, &rstate);
